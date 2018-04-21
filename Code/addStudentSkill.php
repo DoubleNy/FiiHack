3adb1647926491844f105a0683392df726ca3b4c
DavidMarcu <david.marcu.1997@gmail.com>
2018-04-22 02:39:01 +0300

    addedStudInfo

---
+++ Code/addStudentSkill.php
@@ -0,0 +1,24 @@
+<?php
+      $mysql = new mysqli (
+                    'localhost',
+                    'root',
+                    '',
+                    'FiiDB'
+      );
+              //
+      if($mysql->connect_error){
+        die("Eroare la conectare");
+      }
+
+      $id = $_POST["id"];
+      $skill = $_POST["skill"];
+
+      $qry = "insert into studeskills values ('$id' , '$skill')";
+      mysqli_query($mysql, $qry);
+      echo $id . $skill;
+
+
+      //
+?>
+
+
